7a0af6348f0a5af4ab482a345221b6dcad6885b5
Ryan <ryansaftje123@gmail.com>
2025-10-29 20:26:55 +0700

    Deployment to server

--- frontend-kemjar7/vite.config.js
+++ frontend-kemjar7/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'cs7.netlabdte.com', // ⬅️ tambahkan ini
+      'localhost'
+    ],
+    host: '0.0.0.0', 
+  }
 })
